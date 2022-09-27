94be504bd4c924843869f84cbd4da26157aac573
Muhammed Hamdy <modclover7@gmail.com>
2022-09-27 15:22:28 +0200

    adding ranger

---
+++ ranger/commands.py
@@ -0,0 +1,17 @@
+from __future__ import (absolute_import, division, print_function)
+
+from ranger.api.commands import *
+from ranger_udisk_menu.mounter import mount
+
+import os
+
+class emptytrash(Command):
+    """:empty
+
+    Empties the trash 
+    """
+
+    def execute(self):
+        HOME = os.environ['HOME']
+        self.fm.run(f'trash-empty')
+
